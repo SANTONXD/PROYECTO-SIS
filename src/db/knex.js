4d3458b928cb598bbecc320c7a7a7743e799740a
SANTONXD <santiago1210molina@gmail.com>
2025-11-11 20:43:56 -0500

    Add files via upload

---
+++ src/db/knex.js
@@ -0,0 +1,16 @@
+const knex = require('knex');
+require('dotenv').config();
+
+const db = knex({
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
+  },
+  pool: { min: 0, max: 10 }
+});
+
+module.exports = db;
