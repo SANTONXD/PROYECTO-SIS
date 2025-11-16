4f037615d50c0bd974890d182d30036285ffce6e
SANTONXD <santiago1210molina@gmail.com>
2025-11-16 11:23:10 -0500

    Switch database client from MySQL to PostgreSQL

--- src/db/knex.js
+++ src/db/knex.js
@@ -2,13 +2,10 @@ const knex = require('knex');
 require('dotenv').config();
 
 const db = knex({
-  client: 'mysql2',
+  client: 'pg',
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
   },
   pool: { min: 0, max: 10 }
 });
